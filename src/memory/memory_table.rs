400eb45b8844df31031c594cbeff72aec60b42c9
VaskillerDev <Vaskoooo9241@gmail.com>
2020-06-02 13:02:08 +0300

    add PrintOfState struct, add get_symbol, get_operator, resolve_symbol_operator_get_values_by_range_inclusive for MemoryTable

--- src/memory/memory_table.rs
+++ src/memory/memory_table.rs
@@ -1,9 +1,24 @@
-use crate::memory::memory_machine::MemoryMachine;
 use crate::memory::compared::Compared;
+use crate::memory::memory_machine::MemoryMachine;
 use qdb_ast::ast::types::{BinaryExpr, DataType, DataVar};
 use std::borrow::BorrowMut;
 use std::cmp::Ordering;
 use std::collections::{HashMap, HashSet};
+
+#[derive(Debug, PartialOrd, PartialEq)]
+struct PrintOfState {
+    name: String,
+    values: Vec<DataType>,
+}
+
+impl PrintOfState {
+    fn new(name: &String, values: Vec<DataType>) -> Self {
+        PrintOfState {
+            name: name.to_string(),
+            values,
+        }
+    }
+}
 
 // variable storage
 #[derive(Debug)]
@@ -12,6 +27,59 @@ struct MemoryTable {
 }
 
 impl MemoryTable {
+    // hidden function
+    fn get_symbol(data_type: &DataType) -> Option<&String> {
+        return match data_type {
+            DataType::Symbol(val) => Some(val),
+            _ => None,
+        };
+    }
+
+    fn get_operator(operator: &str) -> fn(&Ordering) -> bool {
+        return match operator {
+            "==" => <DataType as Compared>::eq,
+            "!=" => <DataType as Compared>::neq,
+            ">=" => <DataType as Compared>::eq_or_gr,
+            ">" => <DataType as Compared>::gr,
+            "<=" => <DataType as Compared>::eq_or_le,
+            "<" => <DataType as Compared>::le,
+            _ => panic!("Error in operator detection"),
+        };
+    }
+
+    fn resolve_symbol_operator_get_values_by_range_inclusive(
+        &self,
+        maybe_left_symbol: Option<&String>,
+        right: &DataType,
+        operator: &str,
+    ) -> Vec<PrintOfState> {
+        let mut vec: Vec<PrintOfState> = Vec::new();
+
+        if maybe_left_symbol.is_some() {
+            let value = maybe_left_symbol.unwrap();
+
+            let maybe_mem_machine = self.mem.get(value);
+
+            if maybe_mem_machine.is_some() {
+                let mem_machine = maybe_mem_machine.unwrap();
+                let predicate = Self::get_operator(operator);
+
+                let indexes = mem_machine.get_by_compare_with(right, |this, other| {
+                    DataType::comparing(this, other, predicate)
+                });
+
+                // todo: rename
+                let indexes = *indexes.get(0).unwrap(); // why .get(0) ?, what wrong ?
+                for (key, mem) in self.mem.iter() {
+                    let data_types = mem.get_values_by_range_inclusive(indexes);
+                    vec.push(PrintOfState::new(key, data_types));
+                }
+            }
+        }
+        return vec;
+    }
+
+    // public function
     pub fn init() -> Self {
         MemoryTable {
             mem: HashMap::new(),
@@ -49,53 +117,35 @@ impl MemoryTable {
         }
         None
     }
-    pub fn find_by_predicate(&self, binary_expr: &BinaryExpr) -> Option<Vec<Vec<DataType>>> {
-        fn get_symbol(data_type: &DataType) -> Option<&String> {
-            return match data_type {
-                DataType::Symbol(val) => Some(val),
-                _ => None,
-            };
-        }
-        fn get_operator(operator : &str) -> fn(&Ordering) -> bool {
-            return match operator {
-                "==" => <DataType as Compared>::eq,
-                "!=" => <DataType as Compared>::neq,
-                ">=" => <DataType as Compared>::eq_or_gr,
-                ">" => <DataType as Compared>::gr,
-                "<=" => <DataType as Compared>::eq_or_le,
-                "<" => <DataType as Compared>::le,
-                _ => {panic!("Error in operator detection")}
-            }
-        }
-
+    pub fn find_by_predicate(&self, binary_expr: &BinaryExpr) -> Option<Vec<PrintOfState>> {
         let (left, right, operator) = binary_expr.get();
 
         {
-            let maybe_l_value = get_symbol(left);
-            let maybe_r_value = get_symbol(right);
+            let maybe_l_value = Self::get_symbol(left);
+            let maybe_r_value = Self::get_symbol(right);
 
             if maybe_l_value.is_some() && maybe_r_value.is_some() {
                 return None;
             }
 
-            if maybe_l_value.is_some() {
-                let value = maybe_l_value.unwrap();
-                let maybe_mem_machine = self.mem.get(value);
-                if maybe_mem_machine.is_some() {
-                    let mem_machine = maybe_mem_machine.unwrap();
-                    let predicate = get_operator(operator);
-                    let a = mem_machine.get_by_compare_with(right,|this,other|
-                        DataType::comparing(this,other,predicate)
-                    );
-                  let mut vec : Vec<Vec<DataType>> = Vec::new();
-                  let a =  *a.get(0).unwrap();
-                    for (_,mem) in self.mem.iter() {
-                        vec.push(mem.get_values_by_range_inclusive(a))
-                    }
-                    if vec.is_empty() {return None}
-                    return Some(vec)
-                }
+            let result_from_l = self.resolve_symbol_operator_get_values_by_range_inclusive(
+                maybe_l_value,
+                right,
+                operator,
+            );
+            let result_from_r = self.resolve_symbol_operator_get_values_by_range_inclusive(
+                maybe_r_value,
+                left,
+                operator,
+            );
+
+            if !result_from_l.is_empty() {
+                return Some(result_from_l);
             }
+            if !result_from_r.is_empty() {
+                return Some(result_from_r);
+            }
+            return None;
         }
 
         None
@@ -103,7 +153,7 @@ impl MemoryTable {
 }
 
 mod test {
-    use crate::memory::memory_table::MemoryTable;
+    use crate::memory::memory_table::{MemoryTable, PrintOfState};
     use qdb_ast::ast::types::{BinaryExpr, DataType, DataVar};
 
     #[test]
@@ -132,17 +182,40 @@ mod test {
     #[test]
     fn test_memory_find_by_predicate() {
         let mut mem_table = MemoryTable::init();
-        mem_table.insert("my_val", DataType::Int(10));
-        mem_table.insert("my_val", DataType::Int(10));
+        mem_table.insert("my_val", DataType::Int(101));
+        mem_table.insert("my_val", DataType::Int(101));
         mem_table.insert("my_val2", DataType::Int(64));
         mem_table.insert("my_val2", DataType::Int(32));
+        mem_table.insert("my_val3", DataType::Int(32));
 
         let binary_expr = BinaryExpr::new(
+            DataType::Int(101),
             DataType::Symbol("my_val".to_string()),
-            DataType::Int(10),
             "==".to_string(),
         );
-       let a = mem_table.find_by_predicate(&binary_expr);
-        println!("{:?}",a);
+
+        let vec_print_of_state = mem_table.find_by_predicate(&binary_expr).unwrap();
+
+        debug_assert_eq!(
+            true,
+            vec_print_of_state.contains(&PrintOfState::new(
+                &"my_val".to_string(),
+                vec![DataType::Int(101)]
+            ))
+        );
+        debug_assert_eq!(
+            true,
+            vec_print_of_state.contains(&PrintOfState::new(
+                &"my_val2".to_string(),
+                vec![DataType::Int(32), DataType::Int(64)]
+            ))
+        );
+        debug_assert_eq!(
+            true,
+            vec_print_of_state.contains(&PrintOfState::new(
+                &"my_val3".to_string(),
+                vec![DataType::Int(32)]
+            ))
+        );
     }
 }
